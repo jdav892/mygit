805ca6d7b1dea4f5ebaf5ffbe61981d39bb97384
jdav892 <jadavila9@gmail.com>
2024-10-11 14:35:16 -0400

    adds functions to cat files similar to git cat-files

--- main.py
+++ main.py
@@ -77,8 +77,35 @@ def read_object(sha1_prefix):
         size, len(data))
     return (obj_type, data)
 
-
+def cat_file(mode, sha1_prefix):
+    """Write the contents of (or info about) object with given sha-1 prefix
+    to stdout. If mode is 'commit', 'tree', or 'blob', print raw data bytes
+    of object. If mode is 'size', print the size of the object. If mode is 
+    'type', print the type of the object. If mode is 'pretty', print a 
+    prettified version of the object."""
     
+    obj_type, data = read_object(sha1_prefix)
+    if mode in ['commit', 'tree', 'blob']:
+        if obj_type != mode:
+            raise ValueError('expected object type {}, got {}'.format(
+                mode, obj_type))
+        sys.stdout.buffer.write(data)
+    elif mode == 'size':
+        print(len(data))
+    elif mode == 'type':
+        print(obj_type)
+    #elif mode == 'pretty':
+    #    if obj_type in ['commit', 'blob']:
+    #        sys.stdout.buffer.write(data)
+    #    elif obj_type == 'tree':
+    #        for mode, path, sha1 in read_tree(data=data):
+    #            type_str = 'tree' if stat.S_ISDIR(mode) else 'blob'
+    #            print('{:06o} {} {}\t{}'.format(mode, type_str, sha1, path))
+        #else:
+        #    assert False, 'unhandled object type {!r}'.format(obj_type)
+    else:
+        raise ValueError('unexpected mode {!r}'.format(mode))
+   
      
 def read_index():
     #Read git index file and return list of IndexEntry objects
