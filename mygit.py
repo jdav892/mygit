c4debd5817c34458a97bcb47f989c3bda0fccf6d
jdav892 <jadavila9@gmail.com>
2024-10-17 13:07:26 -0400

    uses argparse library to create command line interface actions similar to that of git

--- mygit.py
+++ mygit.py
@@ -7,6 +7,8 @@ IndexEntry = collections.namedtuple('IndexEntry', [
         'uid', 'gid', 'size', 'sha1', 'flags', 'path',
     ])
     
+GIT_USERNAME = os.environ.get["GIT_USERNAME"]
+GIT_PASSWORD = os.environ.get["GIT_PASSWORD"]
 
 
 class ObjectType(enum.Enum):
@@ -271,7 +273,7 @@ def commit(message, author):
     parent = get_local_main_hash()
     if author is None:
         author = '{} <{}>'.format(
-            os.environ['GIT_AUTHOR_NAME'], os.environ['GIT_AUTHOR_EMAIL'])
+            os.environ.get['GIT_AUTHOR_NAME'], os.environ.get['GIT_AUTHOR_EMAIL'])
     timestamp = int(time.mktime(time.localtime()))
     utc_offset = -time.timezone
     author_time = '{} {} {:02}{:02}'.format(
@@ -457,11 +459,73 @@ def push(git_url, username=None, password=None):
      
     
 if __name__ == "__main__":
-    repo_name = "my_repo"
-    if not os.path.exists(repo_name):
-        init(repo_name)
-    data = b"This works"
-    obj_type = "blob"
+    parser = argparse.ArgumentParser()
+    sub_parsers = parser.add_subparser(dest='command', metavar='command')
+    sub_parsers.required = True
     
-    sha1_hash = hash_objects(data, obj_type)
-    print('SHA-1 hash:', sha1_hash, repo_name)
+    sub_parser = sub_parsers.add_parser('add',
+            help='add files(s) to index')
+    sub_parser.add_argument('paths', nargs='+', metavar='path',
+            help='path(s) of files to add')
+    
+    sub_parser = sub_parsers.add_parser('cat-file',
+            help='display contents of object')
+    valid_modes = ['commit', 'tree', 'blob', 'size', 'type', 'pretty']
+    sub_parser.add_argument('mode', choices=valid_modes,
+            help="object type (commit, tree, blob) or display mode (size, "
+            'type, pretty)')
+    sub_parser.add_argument('hash_prefix',
+            help="SHA-1 hash (or hash prefix) of object to display")
+    
+    sub_parser = sub_parsers.add_parser('commit',
+            help='commit current state of index to main branch')
+    sub_parser = add_argument('-a', '--author',
+            help='commit author in format "A U Thor <author@example.com"'
+            '(Uses GIT_AUTHOR_NAME and GIT_AUTHOR_EMAIL environment '
+            'variables by default)')
+    sub_parser.add_argument('-m', '--message', required=True,
+            help="text of commit message")
+    
+    sub_parser = sub_parsers.add_parser('diff',
+            help='show diff of files changed')
+    
+    sub_parser = sub_parsers.add_parser('hash-object',
+            help='hash contents of given path (and optionally write to'
+            'object store)')
+    sub_parser.add_argument('path',
+            help="path of file to hash")
+    sub_parser.add_argument('-t', choices=['commit', 'tree', 'blob'],
+            default='blob', dest='type',
+            help='type of object (default %(default)r)')
+    sub_parser.add_argument('-w', action='store_true', dest='write',
+            help='write object to object store (as well as printing hash)')
+    
+    sub_parser = sub_parsers.add_parser('init',
+            help='initializing a new repo')
+    sub_parser.add_argument('repo',
+            help="directory name for new repo")
+    
+    sub_parser = sub_parsers.add_parser('ls-files',
+            help='list files in index')
+    sub_parser.add_argument('-s', '--stage', action='store_true',
+            help='show object details(mode, hash, and stage number) in'
+            'addition to path')
+    
+    sub_parser = sub_parsers.add_parser('push',
+            help='push main branch to given git server URL')
+    sub_parser.add_argument('git_url',
+            help='URL of git repo, eg: https://github.com/jdav892/mygit.git')
+    sub_parser.add_argument('-p', '--password',
+            help='password to use for authentication (uses GIT_PASSWORD)'
+            'environment variable by default')
+    sub_parser.add_argument('-u', '--username',
+            help='username to use for authentication (uses GIT_USERNAME)'
+            'environment variable by default')
+    
+    sub_parser = sub_parsers.add_parser('status',
+            help='show status of working copy')
+    
+    args = parser.parse_args()
+    
+    
+    
