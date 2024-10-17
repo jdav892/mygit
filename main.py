ac9481227627df859074bdecc95332f7c3f1780b
jdav892 <jadavila9@gmail.com>
2024-10-17 11:33:43 -0400

    reads tree object with given hex xtring or data and returns tuples based on mode path and sha1

--- main.py
+++ main.py
@@ -346,7 +346,27 @@ def get_remote_master_hash(git_url, username, password):
     assert len(main_sha1) == 40
     return main_sha1.decode()
 
- 
+def read_tree(sha1=None, data=None):
+    """Read tree object with given sha1(hex string) or data, and return
+    of (mode, path, sha1) tuples."""
+    if sha1 is not None:
+        obj_type, data = read_object(sha1)
+        assert obj_type  == 'tree'
+    elif data is None:
+        raise TypeError('must specify "sha1" or "data"')
+
+    i = 0
+    entries = []
+    for _ in range(1000):
+        end = data.find(b'\x00', i)
+        if end == -1:
+            break
+        mode_str, path = data[i:end].decode().split()
+        mode = int(mode_str, 8)
+        digest = data[end + 1:end + 21]
+        entries.append((mode, path, digest.hex()))
+        i = end + 1 + 20
+    return entries
 
 if __name__ == "__main__":
     repo_name = "my_repo"
